c4c929aca6682f1cec9d0adce9e4503ec02c4375
GregoryVinicius <gregoryviniciuscla@gmail.com>
2023-06-05 16:59:22 -0300

    Aula 05/06

--- temperatura_umidade/temperatura_umidade.ino
+++ temperatura_umidade/temperatura_umidade.ino
@@ -1,43 +1,61 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
-
+#include <DHT.h>
 //Definicao dos topicos
-const char *topic_pub_distancia = "distancia_da_RE";
+const char *topic_pub_temp = "info_temp";
+const char *topic_pub_umidade = "info_umidade";
 
 //Configuracao do WiFi
 const char *ssid = "Alunos";       //SSID da rede WiFi
 const char *password = "ifprpvai";  //Senha da rede WiFi
 
 //Configuracao do Broker MQTT
-const char *mqtt_id = "Testesensorre_ESP32";   //Identificacao do dispositivo
+const char *mqtt_id = "TesteDHT_ESP32";   //Identificacao do dispositivo
 const char *mqtt_server = "broker.hivemq.com";  //Endereço do servidor MQTT
 const int mqtt_port = 1883;                     //Porta do Broker MQTT
 
 //Variáveis e objetos globais
 WiFiClient espClient;  //Criacao do objeto espClient
 PubSubClient client(espClient);
-int leitura = 0;
 
+#define DHTPIN 32
+#define DHTTYPE DHT11 //Sensor em utilização 
+DHT dht(DHTPIN, DHTTYPE);
 void setup() {
-  pinMode(25, OUTPUT);
-  pinMode(26, OUTPUT);
-  pinMode(27, OUTPUT);
+ pinMode (32, INPUT);
 
   client.setServer(mqtt_server, mqtt_port);
   Serial.begin(115200);
 }
+float read_temp(){
+  float t = dht.readTemperature();
+  float result;
+  if(!(isnan(t))){
+  result = t;
+  }
+  else{
+    result = -99.99;
+  }
+  Serial.print("Temperatura: ");
+  Serial.println(result);
+  return result;
 
-int ReadUltrasonicDistance(int triggerPin, int echoPin) {
-  pinMode(triggerPin, OUTPUT);
-  digitalWrite(triggerPin, LOW);
-  delay(2);
-  digitalWrite(triggerPin, HIGH);
-  delay(10);
-  digitalWrite(triggerPin, LOW);
-  pinMode(echoPin, INPUT);
-  return (int)(0.01723 * (double)pulseIn(echoPin, HIGH));
 }
 
+float read_umi(){
+  float h = dht.readHumidity();
+  float result;
+  if(!(isnan(h))){
+  result = h;
+  }
+  else{
+    result = -99.99;
+  }
+  Serial.print("Umidade: ");
+  Serial.println(result);
+  return result;
+
+}
 //Efetua conexao com o WiFi
 void connect_wifi() {
   Serial.print("Conectando-se a rede: ");
@@ -77,51 +95,25 @@ void keep_connection() {
 }
 
 void loop() {
-  int compare = ReadUltrasonicDistance(18, 19);
 
-  if (compare < 300) {
-  leitura = compare;
-  Serial.println(leitura);
-  }
-  
-  if (leitura > 34) {
-    //apaga os LEDs se necessário
-    digitalWrite(25, LOW);
-    digitalWrite(26, LOW);
-    digitalWrite(27, LOW);
-  }
-  
-  if (leitura > 25 & leitura < 35) {
-    //apaga os LEDs se necessário
-    digitalWrite(25, LOW);
-    digitalWrite(26, LOW);
-    
-    digitalWrite(27, HIGH);
-  }
-  
-  if (leitura > 15 & leitura < 26) {
-    //apaga os LEDs se necessário
-    digitalWrite(25, LOW);
-    
-    digitalWrite(26, HIGH);
-    digitalWrite(27, HIGH);
-  }
-  
-  if (leitura < 16) {
 
-    digitalWrite(25, HIGH);
-    digitalWrite(26, HIGH);
-    digitalWrite(27, HIGH);
-  }
+  
 
-  keep_connection();
+ keep_connection();
 
-  char distance_str[10];
-  sprintf(distance_str, "%d", leitura);
+  char temperatura_str[10];
+  sprintf(temperatura_str, "%.2fc", read_temp());
 
   //Envia as strings ao dashboard MQTT
-  client.publish(topic_pub_distancia, distance_str);
+  client.publish(topic_pub_temp, temperatura_str);
 
+  char umidade_str[10];
+  sprintf(umidade_str, "%.2fc", read_umi());
+
+  //Envia as strings ao dashboard MQTT
+  client.publish(topic_pub_umidade, umidade_str);
   client.loop();
   delay(100);
 }
+
+
